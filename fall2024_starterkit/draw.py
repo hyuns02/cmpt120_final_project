d159e611673e3d05243f1a480e5208077be8b7f0
hyuns02 <hyuns02@outlook.com>
2024-11-19 22:23:01 -0800

    Clean the code

--- fall2024_starterkit/draw.py
+++ fall2024_starterkit/draw.py
@@ -11,28 +11,20 @@ def recolor_image(img, color):
   return False
 
 def minify(img):
-  # Get height and width of the img
+  # Get height and width of the img and creat canvas
   height = len(img)
   width = len(img[0])
+  result_image = cmpt120image.get_white_image(width//2,height//2)
   
   # Reduce both height and width
-  result = []
   for row in range(0,height,2):
-    list_of_pixel = []
     for col in range(0,width,2):
       list_of_rgb = []
       for i in range(3):
         avg = int((img[row][col][i] + img[row][col+1][i] + 
                    img[row+1][col][i] + img[row+1][col+1][i])/4)
         list_of_rgb.append(avg)
-      list_of_pixel.append(list_of_rgb)
-    result.append(list_of_pixel)
-
-  # Create new image
-  result_image = cmpt120image.get_white_image(width//2,height//2)
-  for row in range(height//2):
-    for col in range(width//2):
-      result_image[row][col] = result[row][col]
+      result_image[row//2][col//2] = list_of_rgb
   return result_image
 
 def mirror(img):
