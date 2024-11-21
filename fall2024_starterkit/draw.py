c6dd13bf63076b804df812963685153cf18fd5c0
Jackson <jleekitchener@gmail.com>
2024-11-20 19:52:19 -0800

    Update draw.py

--- fall2024_starterkit/draw.py
+++ fall2024_starterkit/draw.py
@@ -8,52 +8,73 @@
 import cmpt120image as ci
 ci.init()
 
+# check if the colour of the pixcel is black
 def colour_check(pixcel):
   r,g,b = pixcel
-  if r == 0 and g == 0 and b == 0:
-    return True
-  else:
-    return False
+  return r == 0 and g == 0 and b == 0
 
-
-# Note: delete all `return False` lines as you complete each function!
 def recolor_image(img, color):
   height = len(img)
   width = len(img[0])
-  child = ci.get_image("child.png")
+  photo = ci.get_image(img)
 
+# changing the colour of the pixcel if the colour is black into colour that user want
   for row in range(height):
     for col in range(width):
-      pixcel_from_image = child[row][col]
+      pixcel_from_image = photo[row][col]
       if colour_check(pixcel_from_image):
-        child[row][col] = color
-        ci.get_white_image
-  return child
+        photo[row][col] = color
 
-  # ci.show_image(child, "merged")
-  # input("Please any key to end to process")
-
+# creating the new photo with changed colour
+  result_img = ci.get_white_image()
+  for row in range(height):
+      for col in range(width):
+        result_img[row][col] = photo[row][col]
+  return result_img
+  
 
 def minify(img):
-  # Get height and width of the img and create canvas
-  img_height = len(img)
-  img_width = len(img[0])
-  result_image = ci.get_white_image(img_width, img_height)
-
+  # Get height and width of the img and creat canvas
+  height = len(img)
+  width = len(img[0])
+  result_image = ci.get_white_image(width//2,height//2)
   
   # Reduce both height and width
-  for row in range(0,img_height,2):
-    for col in range(0,img_width,2):
+  result = []
+  for row in range(0,height,2):
+    for col in range(0,width,2):
       list_of_rgb = []
       for i in range(3):
         avg = int((img[row][col][i] + img[row][col+1][i] + 
                    img[row+1][col][i] + img[row+1][col+1][i])/4)
         list_of_rgb.append(avg)
-      result_image[row//2][col//2] = list_of_rgb
+
+  # Create new image
+  result_image = ci.get_white_image(width//2,height//2)
+  for row in range(height//2):
+    for col in range(width//2):
+      result_image[row][col] = result[row][col]
   return result_image
 
 def mirror(img):
   height = len(img)
+  width = len(img[0])
+  photo = ci.get_image(img)
+# mirroring the photo
+  for row in range(height):
+    for col in range(height//2):
+      opposite_col = width - col - 1
+      photo[row][col], photo[row][opposite_col] = photo[row][opposite_col], photo[row][col]
+
+# creating new reult img 
+  result_img = ci.get_white_image()
+  for row in range(height):
+    for col in range(width):
+      opposite_col = width - col - 1
+      result_img[row][col] = result_img[row][opposite_col]
+  return result_img
+
+
   
 def draw_item(canvas, item, row, col):
   # Get height and width of the item
